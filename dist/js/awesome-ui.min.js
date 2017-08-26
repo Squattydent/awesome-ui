2f846fd032f2720d7d1b5268c5ec2e23da4cd23f
Kidigo <squattydent@gmail.com>
2017-08-26 12:41:58 +0700

    Awesome UI v1.0.0 - Beta

---
+++ dist/js/awesome-ui.min.js
@@ -0,0 +1,20 @@
+/*
+ * # AWESOME UI - 1.0.0
+ * https://github.com/kidigo/awesome-ui/
+ * https://www.ald.web.id/
+ *
+ * Copyright 2017 Contributors
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ *
+ */
+
+/*!
+ * # AWESOME UI - 1.0.0 - Default
+ * https://github.com/kidigo/awesome-ui/
+ *
+ *
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ *
+ */
